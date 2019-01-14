f6f709a4fce84e5d1118a4064e385f0d9148a14f
Ron Tarrant <rontarrant@gmail.com>
2019-01-14 07:08:14 -0500

    rewrote image_028_button.d as OOP

--- 006_image/image_028_button.d
+++ 006_image/image_028_button.d
@@ -13,9 +13,7 @@ void main(string[] args)
 	Main.init(args);
 	TestRigWindow myTestRig = new TestRigWindow("Test Rig");
 	
-	Button myButt = new Button();
-	Image myImage = new Image("images/apples.jpg");                // *** NEW ***
-	myButt.add(myImage);                                           // *** NEW ***
+	ImageButton myButt = new ImageButton("images/apples.jpg");     // *** NEW ***
 	myTestRig.add(myButt);
 	
 	// Show the window and its contents...
@@ -25,6 +23,7 @@ void main(string[] args)
 	Main.run();
 	
 } // main()
+
 
 class TestRigWindow : MainWindow
 {
@@ -48,3 +47,26 @@ class TestRigWindow : MainWindow
 	} // quitApp()
 
 } // class myAppWindow
+
+
+class ImageButton : Button                                        // *** NEW ***
+{
+	this(string imageName)
+	{
+		super();
+		
+		Image oranges = new Image(imageName);
+		add(oranges);
+		
+		addOnClicked(&doSomething);
+		
+	} // this()
+	
+	
+	void doSomething(Button b)
+	{
+		writeln("You have added one (1) orange to your cart.");
+		
+	} // doSomething()
+	
+} // class ImageButton
